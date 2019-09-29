b4848309377af36f0f1a7c8567b78c3be98a060d
Alfian Losari <alfianlosari@gmail.com>
2019-09-29 14:51:06 +0700

    Initial Commit

---
+++ TopMovies WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TopMovies WatchKit Extension
+//
+//  Created by Alfian Losari on 29/09/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TopMovies WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  TopMovies WatchKit Extension
+//
+//  Created by Alfian Losari on 29/09/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
